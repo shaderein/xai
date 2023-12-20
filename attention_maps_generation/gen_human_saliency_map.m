a9379bf3a912c25d03036a25db584244ebe3bb52
shaderein_abc <shaderein@hotmail.com>
2023-12-21 06:19:51 +0800

    [code] generate human attention maps for images with varied sizes

--- attention_maps_generation/gen_human_saliency_map.m
+++ attention_maps_generation/gen_human_saliency_map.m
@@ -4,49 +4,81 @@
 
 clear;
 
-% DET Hum
+% % DET Hum
 % Path.RawDataPath = 'H:\Projects\HKU_XAI_Project\XAI_Similarity_1\Hum_IdTask_Split_Fixation';
 % Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231206 Hum DET\whole_image';
 % Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231206 Hum DET\whole_image_visualize';
 % Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\bdd\images\orib_hum_id_task_resized\';
 
-% EXP Hum
-Path.RawDataPath = 'H:\OneDrive - The University Of Hong Kong\bdd\results\explanation\231018_vehicle_whole_screen_vb_fixed_pos\exp_fixations_split_by_img';
-Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231219 Veh EXP\attention_maps';
-Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231219 Veh EXP\visualize';
-Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\bdd\images\orib_veh_id_task_resized\';
+% % EXP Hum
+% Path.RawDataPath = 'H:\OneDrive - The University Of Hong Kong\bdd\results\explanation\231018_vehicle_whole_screen_vb_fixed_pos\exp_fixations_split_by_img';
+% Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231219 Veh EXP\attention_maps';
+% Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\bdd\attention_maps\231219 Veh EXP\visualize';
+% Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\bdd\images\orib_veh_id_task_resized\';
 
+% COCO PV
+Path.RawDataPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\fixation\split_by_id\PV\';
+Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_PV_raw\attention_maps\';
+Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_PV_raw\visualize\';
+Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\images\resized\DET\';
+
+% COCO DET
+Path.RawDataPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\fixation\split_by_id\DET\';
+Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_DET_resized\attention_maps\';
+Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_DET_resized\visualize\';
+Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\images\resized\DET\';
+
+% % COCO EXP
+% Path.RawDataPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\fixation\split_by_id\PV\';
+% Path.matSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_PV_raw\attention_maps\';
+% Path.picSavePath = 'H:\OneDrive - The University Of Hong Kong\mscoco\attention_maps\231221_PV_raw\visualize\';
+% Path.backgroundPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\images\resized\DET\';
+
+
+imginfoPath = 'H:\OneDrive - The University Of Hong Kong\mscoco\image_info\image_info_actualarea.xlsx';
+imginfo = readtable(imginfoPath);
 
 Path.CodePath = pwd;
 addpath(genpath(Path.CodePath));
 
 CheckIfDirExist(Path);
 
-fileDir = dir(fullfile(Path.RawDataPath, ['*.xlsx']));
+for i=1:height(imginfo)
+    img_name = strrep(imginfo{i,'StimuliID'}{1},'.png','');
 
-for i = 1:numel(fileDir)
+    intA = xlsread([Path.RawDataPath img_name '.xlsx']);
 
-    curDir = fullfile(fileDir(i).folder, fileDir(i).name);
-    
-    intA = xlsread(curDir);
+    img_path = [Path.backgroundPath img_name '.png'];
 
-    img_path = [Path.backgroundPath strrep(fileDir(i).name, 'xlsx', 'jpg')]
-    
-    opt.gaussian_smooth_val = 30;   
-    [output_map] = gen_heatmap_single(intA(:,1:3), img_path, opt);
+    % Realign fixations to the top left corner of the original, not resized
+    % images. Read width and height data to limit the region on which
+    % saliency maps are generated
+
+    % Check if resized image is in different size than raw image
+    raw_size = size(imread(img_path));
+    % if ~((imginfo{i,'height'}==raw_size(1)) && (imginfo{i,'width'}==raw_size(2)))
+    %     fprintf('%s\tRaw: %dx%d Area:%dx%d',img_name,raw_size(1),raw_size(2),imginfo{i,'height'},imginfo{i,'width'});
+    %     continue
+    % end
+
+    intA(:,1) = intA(:,1) - imginfo{i,'Xlo'}; % FixX
+    intA(:,2) = intA(:,2) - imginfo{i,'Ylo'}; % FixY
+
+    opt.gaussian_smooth_val = 21;   
+    [output_map] = gen_heatmap_single(intA(:,1:3), img_path,imginfo{i,'height'},imginfo{i,'width'},opt);
     output_map_norm = (output_map-min(output_map,[],'all'))./(max(output_map,[],'all')-min(output_map,[],'all'));
     
     % save mat file
-    save(fullfile(Path.matSavePath, [fileDir(i).name(1:end-5) '_GSmo_' num2str(opt.gaussian_smooth_val) '.mat']), 'output_map_norm');
+    save(fullfile(Path.matSavePath, [img_name '_GSmo_' num2str(opt.gaussian_smooth_val) '.mat']), 'output_map_norm');
 
     % save plots
-    copyfile('dataset1picedge.tiff', fullfile(Path.picSavePath, [fileDir(i).name(1:end-5) '_GSmo_' num2str(opt.gaussian_smooth_val) '.tiff']))
+    copyfile('dataset1picedge.tiff', fullfile(Path.picSavePath, [img_name '_GSmo_' num2str(opt.gaussian_smooth_val) '.tiff']))
 
 end
 
 
 %% Support Functions
-function output_map = gen_heatmap_single(Sub, img_path, opt)
+function output_map = gen_heatmap_single(Sub, img_path, height, width, opt)
 
 d1 = size(Sub,1);
 
@@ -59,8 +91,8 @@ summary(:,4) = Sub(:,2);
 save('data1.mat','summary');
 
 cfg=[];
-cfg.xSize= 576; %height of the image, in pixel
-cfg.ySize=1024; %wideth of the image, in pixel
+cfg.xSize= height; %height of the image, in pixel
+cfg.ySize= width; %width of the image, in pixel
 cfg.columnx=3;
 cfg.columny=4;
 cfg.columnduration=2;
