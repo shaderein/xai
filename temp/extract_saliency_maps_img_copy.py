a7c7b969c1e77b7e95d0b0ad6a68116e92ecc80d
shaderein_abc <shaderein@hotmail.com>
2024-09-06 15:19:37 +0800

    add visualization examples and temp scripts (helpers)

---
+++ temp/extract_saliency_maps_img_copy.py
@@ -0,0 +1,138 @@
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+import os, re, shutil
+
+import cv2
+
+def combine_images(root_dir, target_img_name, ext):
+
+    model = 'fasterrcnn'
+    title_font_size = 15
+
+    layers = [
+        ['backbone.res2.0.conv1',
+        'backbone.res2.0.conv2',
+        'backbone.res2.0.conv3',
+        'backbone.res2.1.conv1',
+        'backbone.res2.1.conv2',
+        'backbone.res2.1.conv3',],
+        ['backbone.res2.2.conv1',
+        'backbone.res2.2.conv2',
+        'backbone.res2.2.conv3',
+        'backbone.res3.0.conv1',
+        'backbone.res3.0.conv2',
+        'backbone.res3.0.conv3',],
+        ['backbone.res3.1.conv1',
+        'backbone.res3.1.conv2',
+        'backbone.res3.1.conv3',
+        'backbone.res3.2.conv1',
+        'backbone.res3.2.conv2',
+        'backbone.res3.2.conv3',],
+        ['backbone.res3.3.conv1',
+        'backbone.res3.3.conv2',
+        'backbone.res3.3.conv3',
+        'backbone.res4.0.conv1',
+        'backbone.res4.0.conv2',
+        'backbone.res4.0.conv3',],
+        ['backbone.res4.1.conv1',
+        'backbone.res4.1.conv2',
+        'backbone.res4.1.conv3',
+        'backbone.res4.2.conv1',
+        'backbone.res4.2.conv2',
+        'backbone.res4.2.conv3',],
+        ['backbone.res4.3.conv1',
+        'backbone.res4.3.conv2',
+        'backbone.res4.3.conv3',
+        'backbone.res4.4.conv1',
+        'backbone.res4.4.conv2',
+        'backbone.res4.4.conv3',],
+        ['backbone.res4.5.conv1',
+        'backbone.res4.5.conv2',
+        'backbone.res4.5.conv3',],
+
+        #ROI
+        ['roi_heads.pooler.level_poolers.0',],
+        ['roi_heads.res5.0.conv1',
+        'roi_heads.res5.0.conv2',
+        'roi_heads.res5.0.conv3',
+        'roi_heads.res5.1.conv1',
+        'roi_heads.res5.1.conv2',
+        'roi_heads.res5.1.conv3',],
+        ['roi_heads.res5.2.conv1',
+        'roi_heads.res5.2.conv2',
+        'roi_heads.res5.2.conv3',]
+    ]
+
+    rows = len(layers)
+    cols = max([len(c) for c in layers])
+    fig, axes = plt.subplots(rows,cols,figsize=(20,20))
+
+    layer_idx = 1
+
+    for i in range(len(layers)):
+        for j in range(len(layers[i])):
+            layer = layers[i][j]
+            for dir in os.listdir(root_dir):
+                if ".mat" in dir: continue
+                if layer in dir:
+                    image_path = os.path.join(root_dir,dir,f'{target_img_name}-res.{ext}')
+                    image = cv2.imread(image_path)
+                    if 'odam' in image_path:
+                        if 'vehicle' in image_path or 'human' in image_path:
+                            crop_img = image[:,round(image.shape[1]*1/3):round(image.shape[1]*2/3),:]
+                        else:
+                            crop_img = image[:,round(image.shape[1]*2/4):round(image.shape[1]*3/4),:]
+                    elif 'fullgradcamraw' in image_path:
+                        crop_img = image[:,round(image.shape[1]/2):,:]
+                    ind_maps_dir = f"results/visualizations/all_maps_{root_dir.split('/')[-1]}/"
+                    if not os.path.exists(ind_maps_dir):
+                        os.makedirs(ind_maps_dir)
+                    cv2.imwrite(os.path.join(ind_maps_dir,f"{layer}_{target_img_name}.png"),crop_img)
+
+                    crop_img = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
+                    axes[i,j].imshow(crop_img)
+                    if "pooler" in layer:
+                        axes[i,j].set_title(f"[{layer_idx}] {layer}",fontsize=int(title_font_size*0.8))
+                    else:
+                        axes[i,j].set_title(f"[{layer_idx}] {layer}",fontsize=title_font_size)
+                    axes[i,j].axis('off')
+                    layer_idx += 1
+                    break
+
+        #Hide any unused axes in the current row
+        for j in range(len(layers[i]), cols):
+            fig.delaxes(axes[i, j])
+
+    plt.tight_layout()
+    plt.savefig(f"results/visualizations/{model}_{root_dir.split('/')[-1]}_{target_img_name}.png")
+
+# root_dir = "/mnt/h/OneDrive - The University Of Hong Kong/mscoco/xai_saliency_maps_faster/fullgradcamraw"
+# target_img_name = 'chair_81061'
+# ext = 'png'
+# combine_images(root_dir,target_img_name,ext)
+
+root_dir = "/mnt/h/jinhan/results/mscoco/xai_saliency_maps_faster_gaussian_sigma4/fullgradcamraw"
+target_img_name = 'chair_81061'
+ext = 'png'
+combine_images(root_dir,target_img_name,ext)
+
+# root_dir = "/mnt/h/OneDrive - The University Of Hong Kong/bdd/xai_saliency_maps_same_layer_faster/fullgradcamraw_human"
+# target_img_name = '760'
+# ext = 'jpg'
+# combine_images(root_dir,target_img_name,ext)
+
+# root_dir = "/mnt/h/OneDrive - The University Of Hong Kong/bdd/xai_saliency_maps_same_layer_faster/fullgradcamraw_human"
+# target_img_name = '34'
+# ext = 'jpg'
+# combine_images(root_dir,target_img_name,ext)
+
+# root_dir = "/mnt/h/OneDrive - The University Of Hong Kong/bdd/xai_saliency_maps_same_layer_faster/fullgradcamraw_vehicle"
+# target_img_name = '940'
+# ext = 'jpg'
+# combine_images(root_dir,target_img_name,ext)
+
+# root_dir = "/mnt/h/OneDrive - The University Of Hong Kong/bdd/xai_saliency_maps_same_layer_faster/fullgradcamraw_vehicle"
+# target_img_name = '180'
+# ext = 'jpg'
+# combine_images(root_dir,target_img_name,ext)
---
+++ temp/plot_saliency_maps.py
@@ -0,0 +1,102 @@
+import matplotlib.pyplot as plt
+import os, re, torch,cv2
+import scipy.io
+from collections import defaultdict
+import numpy as np
+import pandas as pd
+
+# from ..yolov5BDD.utils.util_my_yolov5 import load_gt_labels
+# TODO: use relative import
+def xywh2xyxy(x):
+    # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
+    y = torch.zeros_like(x) if isinstance(x, torch.Tensor) else np.zeros_like(x)
+    y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
+    y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
+    y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
+    y[:, 3] = x[:, 1] + x[:, 3] / 2  # bottom right y
+    return y
+
+def concat_images(images):
+    w, h = images[0].shape[:2]
+    width = w
+    height = h * len(images)
+    base_img = np.zeros((width, height, 3), dtype=np.uint8)
+    for i, img in enumerate(images):
+        base_img[:, h * i:h * (i + 1), ...] = img
+    return base_img
+
+def load_gt_labels(img, label_path, class_names_gt, class_names_sel):
+    label_data = np.loadtxt(label_path, dtype=np.float32, delimiter=' ')
+    if len(label_data.shape) == 1:
+        label_data = label_data[None,:]
+    label_data_class = label_data[:, 0]
+    label_data_corr = label_data[:,1:]
+    sel_idx = []
+    for class_name_sel in class_names_sel:
+        sel_idx.append(class_names_gt.index(class_name_sel))
+    sel_bbox_idx = []
+    label_data_class_names = []
+    for i, i_label_data_class in enumerate(label_data_class):
+        if i_label_data_class in sel_idx:
+            sel_bbox_idx.append(i)
+            label_data_class_names.append(class_names_gt[i_label_data_class.astype('int32')])
+    label_data_corr = label_data_corr[sel_bbox_idx,:] #filter classes
+    label_data_class = label_data_class[sel_bbox_idx] #filter class labels
+    img_h, img_w = np.size(img, 0), np.size(img, 1)
+    label_data_corr[:, 0] = label_data_corr[:, 0] * img_w
+    label_data_corr[:, 1] = label_data_corr[:, 1] * img_h
+    label_data_corr[:, 2] = label_data_corr[:, 2] * img_w
+    label_data_corr[:, 3] = label_data_corr[:, 3] * img_h
+    label_data_corr_xywh = label_data_corr
+    label_data_corr = xywh2xyxy(label_data_corr)
+    label_data_corr_xyxy = label_data_corr
+    label_data_corr_yxyx = label_data_corr[:, [1,0,3,2]]
+    label_data_corr_yxyx = np.round(label_data_corr_yxyx)
+    boxes_GT = label_data_corr_yxyx[:, None, :].tolist()
+
+    return boxes_GT, label_data_corr_xyxy, label_data_corr_xywh, label_data_corr_yxyx, label_data_class, label_data_class_names
+
+def get_res_img(mask, res_img):
+    mask = torch.from_numpy(mask)
+    mask = mask.mul(255).add_(0.5).clamp_(0, 255).numpy().astype(np.uint8)
+    heatmap = cv2.applyColorMap(mask, cv2.COLORMAP_JET)
+    heatmap = (heatmap/255).astype(np.float32)
+    #n_heatmat = (Box.fill_outer_box(heatmap, bbox) / 255).astype(np.float32)
+    res_img = (res_img / 255).astype(np.float32)
+    res_img = cv2.add(res_img, heatmap)
+    res_img = (res_img / res_img.max())
+    return res_img, heatmap
+
+
+
+img = np.zeros([576,1024])
+
+# input path
+images_path = '/mnt/h/Projects/HKU_XAI_Project/Yolov5self_GradCAM_Pytorch_1/orib_veh_id_task0922'
+
+# Whole-image Attention Maps
+human_attention_path = '/mnt/h/OneDrive - The University Of Hong Kong/bdd/attention_maps/231206 Veh DET/whole_image'
+
+# Output path
+plot_path = '/mnt/h/OneDrive - The University Of Hong Kong/bdd/attention_maps/231206 Veh DET/visualize_test'
+
+human_attention = defaultdict()
+
+for file in os.listdir(human_attention_path):
+    if '.mat' not in file: continue
+    img_idx = int(re.findall(r'\d+_',file)[-1].replace('_',''))
+    mat = scipy.io.loadmat(os.path.join(human_attention_path,file))
+
+    map = mat['output_map_norm']
+    
+    human_attention[img_idx] = map
+
+    # Visualize
+    img_path = os.path.join(images_path, f'{img_idx}.jpg')
+    img = cv2.imread(img_path)
+
+    # Plot saliency maps
+    res_img_whole, _ = get_res_img(map, img.copy())
+
+    final_image = res_img_whole*255
+    cv2.imwrite(os.path.join(plot_path,f"{img_idx}.jpg"), final_image)
